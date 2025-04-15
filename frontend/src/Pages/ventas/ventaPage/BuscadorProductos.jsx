0429c91fcc1e82d7e4b457f7640aa81460347612
Matias340 <mattersandres2000@gmail.com>
2025-04-15 19:24:37 -0300

    inclui escaner

--- frontend/src/Pages/ventas/ventaPage/BuscadorProductos.jsx
+++ frontend/src/Pages/ventas/ventaPage/BuscadorProductos.jsx
@@ -5,6 +5,7 @@ import useProductStore from "../../../store/productStore/productStore";
 function buscadorProductos() {
   const { products, setCurrentProduct } = useProductStore();
   const [searchTerm, setSearchTerm] = useState("");
+  const [barcode, setBarcode] = useState(""); // Agregamos un nuevo estado para el código de barras
 
   const handleSearch = (e) => {
     const searchValue = e.target.value?.toLowerCase(); // Evita error si es undefined
@@ -13,6 +14,27 @@ function buscadorProductos() {
 
     const selectedProduct = products.find(
       (p) => p.name?.toLowerCase() === searchValue
+    );
+
+    if (selectedProduct) {
+      setCurrentProduct({
+        name: selectedProduct.name,
+        price: selectedProduct.price,
+        barcode: selectedProduct.barcode,
+        stockAmount: selectedProduct.stockAmount,
+      });
+    }
+  };
+
+  // Maneja el código de barras
+  const handleBarcodeSearch = (e) => {
+    const barcodeValue = e.target.value;
+    setBarcode(barcodeValue);
+
+    if (!barcodeValue) return; // Si está vacío, salir de la función
+
+    const selectedProduct = products.find(
+      (p) => p.barcode === barcodeValue // Buscamos el producto por código de barras
     );
 
     if (selectedProduct) {
@@ -57,6 +79,16 @@ function buscadorProductos() {
             <option disabled>No hay productos</option>
           )}
         </datalist>
+
+        {/* Buscador de Código de Barras */}
+        <input
+          type="text"
+          value={barcode}
+          onChange={handleBarcodeSearch} // Llamar a la función para buscar por código de barras
+          className="w-0 h-0 opacity-0 absolute z-10"
+          placeholder="Buscar por Código de Barras"
+        />
+
         <button className="bg-blue-600 font-bold text-white px-4 py-2 cursor-pointer shadow-md rounded-md hover:bg-blue-700">
           Agregar
         </button>
